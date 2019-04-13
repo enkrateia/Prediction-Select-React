e3c2531db5c9ad5d17c001b0e450967f5b54d98c
Carlos Govea <cgovea@Carloss-MacBook-Pro-2.local>
2019-04-12 23:40:46 -0500

    Changes to AutoComplete

--- src/AutocompleteApp.js
+++ src/AutocompleteApp.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React from "react";
+import "./AutocompleteApp.css";
+import Predictions from "./Predictions";
 
-import './AutocompleteApp.css';
-import books from './books.js';
+export default class AutocompleteApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { autocomplete: "", coincidences: 0 };
+    this.coincidences = 0;
+  }
 
-export default function AutocompleteApp() {
+  handleChange = event => {
+    this.setState({ autocomplete: event.target.value });
+  };
+
+  handlePrediction = number => {
+    this.coincidences = number;
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.coincidences !== this.coincidences) {
+      this.setState({ coincidences: this.coincidences });
+    }
+  }
+
   // TODO: Use the data in `books` to complete this application.
-  return (
-    <form className="autocomplete-app">
-      <input type="text" autoFocus />
-      <ul className="autocomplete-suggestions">
-        <li>Example suggestion.</li>
-      </ul>
-    </form>
-  );
+  render() {
+    return (
+      <form className="autocomplete-app">
+        <div className="container">
+          <input type="text" autoFocus onChange={this.handleChange} value={this.state.autocomplete} />
+          <span>{`${this.state.coincidences} coincidences`}</span>
+          <div className="element-list">
+            <ul className="autocomplete-suggestions">
+              <Predictions inputValue={this.state.autocomplete} setPredictions={this.handlePrediction} />
+            </ul>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
